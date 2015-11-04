3e25136e9530ed6666dc6912f206777517fb8c48
victor <p.ex1@hotmail.com>
2015-11-04 15:49:09 +0100

    mean

---
+++ config/env/development.js
@@ -0,0 +1,12 @@
+module.exports = {
+  db: 'mongodb://localhost/mean',
+  sessionSecret: 'developmentSessionSecret',
+  google: {
+    clientID: '4270183716-3qirtnj0e5pq7vp2ljvcca1j2nlr92mb.apps.googleusercontent.com',
+    clientSecret: 'evRhbkTzXzTVSzkkCam1EkrD',
+    callbackURL: 'http://localhost:3000/oauth/google/callback'
+  }
+};
+
+
+
---
+++ public/application.js
@@ -0,0 +1,13 @@
+var mainApplicationModuleName = 'REDSOCIAL';
+
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngResource','ngRoute','users','example', 'articles']);
+
+mainApplicationModule.config(['$locationProvider',
+  function($locationProvider) {
+    $locationProvider.hashPrefix('!');
+  }
+]);
+
+angular.element(document).ready(function() {
+  angular.bootstrap(document, [mainApplicationModuleName]);
+});
